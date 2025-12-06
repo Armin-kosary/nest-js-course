93b46640f0c403f1545672a126055999bcc79c22
Armin <armynkosari@gmail.com>
2025-12-06 09:13:08 -0800

    Add <getname> route to controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('getname')
+  getName(): string {
+    return "Hello my name in Armin"
+  }
 }
