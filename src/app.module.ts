b6f6bfae1c35eb05f93cff8a9b61385b3a333627
Armin <armynkosari@gmail.com>
2025-12-07 00:03:27 -0800

    Generate Module File for users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [UsersController],
   providers: [],
 })
