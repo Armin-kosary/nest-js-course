d80870b6721354e633feefaa2facda4651a39001
Armin <armynkosari@gmail.com>
2025-12-07 00:05:32 -0800

    Delete Controllers & Providers from app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
-  controllers: [UsersController],
-  providers: [],
 })
 export class AppModule {}
